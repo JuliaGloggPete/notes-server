5ded27b15b01188522c500a0837b378fa863059b
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2023-11-23 12:07:18 +0100

    signup

---
+++ functions/signUp/index.js
@@ -0,0 +1,67 @@
+const { sendResponse } = require("../../responses")
+const bycrypt = require('bcryptjs');
+const { nanoid } = require("nanoid");
+
+const AWS = require('aws-sdk');
+const db = new AWS.DynamoDB.DocumentClient();
+
+
+
+
+async function createAccount(username, hashedPassword, userId, firstname, lastname){
+
+    try{
+
+await db.put({
+    TableName:'notes-accounts',
+    Item:{
+        username: username,
+        password: hashedPassword,
+        firstname: firstname,
+        lastname: lastname,
+        userId: userId
+
+    }
+}).promise();
+
+return {success:true, userId}
+}catch(error){
+    console.log(error);
+   return {success:false, message: 'Could not create account'}
+}
+
+
+}
+
+async function signUp(username, password, firstname, lastname){
+    // check om den redan finns...
+
+    const hashedPassword = bycrypt.hash(password, 10);
+    const userId = nanoid();
+
+    const result = await createAccount(username, 
+        hashedPassword, userId, firstname,lastname)
+
+        return result;
+
+    
+}
+
+
+
+
+exports.handler = async(event,context)=>{
+
+   const {username, password, firstname, lastname} = JSON.parse(event.body)
+
+   const result = await signUp(username, password, firstname, lastname)
+
+   if(result.success)
+
+   return sendResponse(200, result);
+   else
+   return sendResponse(400, result)
+ 
+ }
+ 
+ 
